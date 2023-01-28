f7bd413c1ae631521f56bfcd99855903878514d1
An T.G <boltcarrier@naver.com>
2023-01-29 08:40:28 +0900

    댓글 기눙 구현 - Controller 작성

---
+++ ch4/src/main/java/com/primarchan/board/controller/CommentController.java
@@ -0,0 +1,95 @@
+package com.primarchan.board.controller;
+
+import com.primarchan.board.domain.CommentDto;
+import com.primarchan.board.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@RestController
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    // 댓글을 수정하는 메서드
+    @PatchMapping("/comments/{cno}")
+    public ResponseEntity<String> modify(@PathVariable Integer cno, @RequestBody CommentDto dto) {
+
+        dto.setCno(cno);
+        System.out.println("dto = " + dto);
+
+        try {
+            int rowCnt = commentService.modify(dto);
+
+            if (rowCnt != 1) {
+                throw new Exception("Modify failed.");
+            }
+            return new ResponseEntity<String>("MOD_OK", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<String>("MOD_ERR", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    // 댓글을 저장(작성)하는 메서드
+    @PostMapping("/comments")  // /comments?bno=1232 [POST]
+    public ResponseEntity<String> write(@RequestBody CommentDto dto, Integer bno, HttpSession session) {
+        // String commenter = (String) session.getAttribute("id");
+        String commenter = "test01";
+        dto.setCommenter(commenter);
+        dto.setBno(bno);
+        System.out.println("dto = " + dto);
+
+        try {
+            int rowCnt = commentService.write(dto);
+
+            if (rowCnt != 1) {
+                throw new Exception("Write failed.");
+            }
+            return new ResponseEntity<String>("WRT_OK", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<String>("WRT_ERR", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 지정된 댓글을 삭제하는 메서드
+    @DeleteMapping("/comments/{cno}")  // /comments/1?bno=1232
+    public ResponseEntity<String> remove(@PathVariable Integer cno, Integer bno, HttpSession session) {
+        // String commenter = (String) session.getAttribute("id");
+        String commenter = "test01";
+
+        try {
+            int rowCnt = commentService.remove(cno, bno, commenter);
+
+            if (rowCnt != 1) {
+                throw new Exception("Delete failed.");
+            }
+
+            return new ResponseEntity<String>("DEL_OK", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<String>("DEL_ERR", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 지정된 게시물의 모든 댓글을 가져오는 메서드
+    @GetMapping("/comments")
+    public ResponseEntity<List<CommentDto>> list(Integer bno) {
+        List<CommentDto> list = null;
+        try {
+            list = commentService.getList(bno);
+            return new ResponseEntity<List<CommentDto>>(list, HttpStatus.OK);  // 200
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<List<CommentDto>>(HttpStatus.BAD_REQUEST);  // 400
+        }
+    }
+
+}
